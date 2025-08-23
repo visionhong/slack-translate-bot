246891a36d75c7f024188aa2743636d9034320ac
visionhong <tjems6498@gmail.com>
2025-08-23 15:59:01 +0900

    Add test endpoint to debug deployment

---
+++ api/test.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        response = {
+            "status": "ok",
+            "message": "Test endpoint working"
+        }
+        
+        self.wfile.write(json.dumps(response).encode())
